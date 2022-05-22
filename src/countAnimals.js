ed31a426fbd963cc5c29a5159b0b720b7fbd88ec
Luiz Junior <lacjunior129.la@gmail.com>
2022-05-21 21:52:20 -0300

    requisito 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+// const [animals] = data.species;
+
+const countAnimals = (animal) => {
+  if (!animal) {
+    return data.species.reduce((acc, curr) => Object.assign(acc, { [curr
+      .name]: curr.residents.length }), {});
+  }
+
+  const [animalName, animalSex] = Object.values(animal);
+
+  const [{ residents }] = data.species.filter(({ name }) => animalName.includes(name));
+
+  return (!animalSex) ? (residents.length) : (residents
+    .filter(({ sex }) => sex === animalSex).length);
+};
 
 module.exports = countAnimals;
