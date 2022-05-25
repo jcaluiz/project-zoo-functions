45b051ca4d9174e37d0a440b2a9cdf8b455cd0ac
Luiz Junior <lacjunior129.la@gmail.com>
2022-05-24 21:34:48 -0300

    refatorando requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -2,20 +2,17 @@ const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
   it('retorna a quantidade de elefantes', () => {
-    const expected = 4;
+    const expected = parseInt(4, 10);
     const actual = handlerElephants('residents').length;
     expect(actual).toEqual(expected);
   });
   it('retorna um array com a relação dos nomes de todos os elefantes', () => {
-    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
-    const [...actual] = handlerElephants('residents').map(({ name }) => name);
-    expect(actual).toStrictEqual(expected);
+    const actual = handlerElephants('names');
+    expect(actual).toContain('Jefferson');
   });
   it('retorna a média de idade dos elefantes', () => {
-    const averageAge = handlerElephants('residents').filter(({ age }) => age)
-      .reduce((acc, curr) => acc + curr.age, 0) / handlerElephants('residents').length;
-    const expected = 10.5;
-    expect(averageAge).toEqual(expected);
+    const actual = handlerElephants('averageAge');
+    expect(actual).toBeCloseTo(10.5);
   });
   it('retorna a localização dos elefantes dentro do Zoológico', () => {
     const expected = 'NW';
@@ -23,13 +20,15 @@ describe('Testes da função HandlerElephants', () => {
     expect(actual).toEqual(expected);
   });
   it('retorna a popularidade dos elefantes', () => {
-    const expected = 5;
     const actual = handlerElephants('popularity');
-    expect(actual).toEqual(expected);
+    expect(actual).toBeGreaterThanOrEqual(5);
   });
   it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
-    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
     const actual = handlerElephants('availability');
-    expect(actual).toEqual(expected);
+    expect(actual).not.toContain('Monday');
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    const actual = handlerElephants();
+    expect(actual).toBeUndefined();
   });
 });
