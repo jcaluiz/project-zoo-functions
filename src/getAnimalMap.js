634b7412c16cef8ebc9b68a6205768d5d04eefc8
Luiz Junior <lacjunior129.la@gmail.com>
2022-05-24 22:58:57 -0300

    comentando requisito 14 para passar os requisitos 15 e 16 por causa do lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,13 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalMap = (options) => {
-  if (!options) {
-    const location1 = data.species.map(({ location }) => location);
-    const animalsLocations = data.species.filter(({ location }) => location1.includes(location));
-    const animalsLocationsSeparate = animalsLocations.map(({ name, location }) => ({ [location]: [name] }));
-    // const locationKeys = Object.values(animalsLocationsSeparate);
-    return animalsLocationsSeparate.filter((element) => location1.includes(element));
-  }
+  // if (!options) {
+  //   const location1 = data.species.map(({ location }) => location);
+  //   const animalsLocations = data.species.filter(({ location }) => location1.includes(location));
+  //   const animalsLocationsSeparate = animalsLocations.map(({ name, location }) => ({ [location]: [name] }));
+  //   // const locationKeys = Object.values(animalsLocationsSeparate);
+  //   return animalsLocationsSeparate.filter((element) => location1.includes(element));
+  // }
 };
 
 console.log(getAnimalMap());
